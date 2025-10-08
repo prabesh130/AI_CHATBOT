69ad690daff863003ca935b05338b1bec6390c8d
prabesh130 <adhikariprabesh828@gmail.com>
2025-10-08 20:18:45 +0545

    login and authentication completed react and django are fully connected

--- Backend/AI_Chatbot/urls.py
+++ Backend/AI_Chatbot/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from accounts.views import home_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- Backend/accounts/serializers.py
+++ Backend/accounts/serializers.py
@@ -2,34 +2,42 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
 
+
 class UserSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(max_length=100)
+
     class Meta:
-        model=User
-        fields=['id','username','email','first_name','last_name']
+        model = User
+        fields = ['username', 'email']
+
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password=serializers.CharField(write_only=True,required=True,validators=[validate_password])
-    password2=serializers.CharField(write_only=True,required=True)
-    email=serializers.EmailField(required=True)
-
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        validators=[validate_password]
+    )
+    password2 = serializers.CharField(write_only=True, required=True)
+    email = serializers.EmailField(required=True)
 
     class Meta:
-        model=User
-        fields=['username','email','password','password2']
-    def validate(self,attrs):
-        if attrs['password']!=attrs['password2']:
-            raise serializers.ValidateError({'passowrd': "passowrd field didn't match"})
-        if User.objects.filter(email=attrs['email']).exist():
-            raise serializers.ValidationError({'email':"Email already exist!"})
+        model = User
+        fields = ['username', 'email', 'password', 'password2']
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({'password': "Password fields didn't match."})
+
+        if User.objects.filter(email=attrs['email']).exists():
+            raise serializers.ValidationError({'email': "Email already exists!"})
+
         return attrs
-    
 
-    def create(self,valdiate_data,validated_data:dict):
-        valdiate_data.pop("password2")
-        user=User.objects.create_user(
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = User.objects.create_user(
             username=validated_data['username'],
-            emaild=validated_data['email'],
+            email=validated_data['email'],
             password=validated_data['password']
-
         )
-        return user 
+        return user
